234baf8effe3de2636d388bf5ed86496291e535f
Amilcar Aponte <apontejaj@gmail.com>
2020-11-06 10:50:28 +0000

    V2
    Version 2 is better than version one.

--- src/gitdemo/GITDemo.java
+++ src/gitdemo/GITDemo.java
@@ -16,6 +16,8 @@ public class GITDemo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Hello world");
     }
     
 }
